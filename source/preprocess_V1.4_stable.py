f2f75babcd361dd1ae8ed15731dd8fd0b628a897
Digitalship <83201044+digital-10@users.noreply.github.com>
2024-12-30 09:31:35 +0900

    스케일링 데이터 저장
    Signed-off-by: Digitalship <83201044+digital-10@users.noreply.github.com>

--- source/preprocess_V1.4_stable.py
+++ source/preprocess_V1.4_stable.py
@@ -538,3 +538,4 @@ if __name__ == '__main__':
                     df_scaled = pd.concat([X_train_scaled, X_test_scaled])
         # 10.4 scaling 저장
                 dest_path = os.path.join(parent, os.path.join('data_preprocessed', f'{folder}'))
+                dest_path = os.path.join(parent, os.path.join(dest_path, 'scaled'))
