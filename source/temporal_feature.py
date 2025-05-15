dc093f5869c12e3aabf4f39005db519a0b1e8a50
digital-10 <seonwons@digitalship.co.kr>
2025-05-15 11:31:14 +0900

    Auto commit on save

--- source/temporal_feature.py
+++ source/temporal_feature.py
@@ -2,3 +2,7 @@ from sklearn.base import BaseEstimator, TransformerMixin
 import pandas as pd
 import numpy as np
 import re
+
+class DateFeatureTransformer2(BaseEstimator, TransformerMixin):
+    
+    )
