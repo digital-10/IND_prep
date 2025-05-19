c02f229b578eb03f97ba4f166114d6e580b5fa97
digital-10 <seonwons@digitalship.co.kr>
2025-05-19 11:16:29 +0900

    Auto commit on save

--- source/preprocess_V1.6.py
+++ source/preprocess_V1.6.py
@@ -131,6 +131,7 @@ print(f'There are {date_cols_len} date_time variables')
 print(f'There are {dict_cols_len} dict variables')
 print(f'There are {vector_cols_len} vector variables')
 print(f'There are {non_dec_cols_len} non-decimal variables')
+print(f'There are {sentence_cols_len} sentence variables')
      
     
 
