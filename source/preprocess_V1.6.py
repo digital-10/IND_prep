001fcad5ed07b1a27190b037b6be001c1fd846ff
digital-10 <seonwons@digitalship.co.kr>
2025-05-19 09:09:54 +0900

    Auto commit on save

--- source/preprocess_V1.6.py
+++ source/preprocess_V1.6.py
@@ -4,3 +4,6 @@ import numpy as np
 import os
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
