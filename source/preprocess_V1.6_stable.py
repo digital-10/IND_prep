e19ced9650e797bdeb4f894fb4dbab9b23f82f1a
digital-10 <seonwons@digitalship.co.kr>
2025-07-24 10:57:54 +0900

    Auto commit on save

--- source/preprocess_V1.6_stable.py
+++ source/preprocess_V1.6_stable.py
@@ -75,7 +75,7 @@ class CustomOrdinalEncoder(BaseEstimator, TransformerMixin):
 class JSONExtractortransformer(BaseEstimator, TransformerMixin):
     def __init__(self, variables: Union[None, List[str]]==None):
         if variables is not None and not isinstance(variables, list):
-            self.variables =[variables]ㅁㄴㅇㄹ
+            self.variables =[variables]
         else:
             self.variables = variables
         self.feature_names_in_ = None
