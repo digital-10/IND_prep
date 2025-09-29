5ce53c3313371d5bda377b6686ffe07309072453
digitalship-lab <xodn4363@gmail.com>
2025-09-29 15:30:18 +0900

    불필요한 주석 제거

--- source/preprocess_V1.6_stable.py
+++ source/preprocess_V1.6_stable.py
@@ -594,7 +594,7 @@ def do_imputation(df,pipe):
 def scaling(df):
     df = df.copy()
     if config_dict['scale'] is np.nan:
-        config_dict['scale'] = ['minmax']
+        config_dict['scale'] = ['minmax'] # default with minmax scaling
     if config_dict['scale'][0] =='minmax':
         scaler = MinMaxScaler()
         scaler.fit(df)
